bb3ea8ae4d61c98439799de835f18e7a1baf0f54
Jakub Sękowski <sequba@gmail.com>
2017-02-20 12:56:17 +0100

    Script showing initial generations
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

---
+++ initials_report.sh
@@ -0,0 +1,10 @@
+# Script runs CA for a number of parameters and presents the results on a big png image
+
+for p in `seq 1 9`
+do
+	echo $p
+	./draw maps/p$p.bmp < maps/p$p.initial
+done
+convert maps/p{1,2,3,4,5,6,7,8,9}.bmp +append maps/report.png
+rm -f maps/current
+rm -f maps/*.bmp
